cf928529cdd399a8fc424b8d5713175c2526febb
oleghe <olegche1977@gmail.com>
2022-09-24 21:47:11 +0300

    Created hello 2

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <div>Hello</div>
+        <div>Hello 2</div>
         <a
           className="App-link"
           href="https://reactjs.org"
